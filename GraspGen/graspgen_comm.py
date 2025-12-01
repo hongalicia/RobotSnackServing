cef80f01c0d7b4e6c6a26f3e9079e0fb714bcb63
mia <mia@gmail.com>
2025-12-01 22:36:51 +0800

    raise exception when graspgen fails

--- GraspGen/graspgen_comm.py
+++ GraspGen/graspgen_comm.py
@@ -7,11 +7,15 @@ class GraspGenCommunication:
     def __init__(self, port_sender=9890, port_receiver=9891):
         self.sender = NonBlockingJSONSender(port=port_sender)
         self.receiever = BlockingJSONReceiver(port=port_receiver)
-
+        
     def send_data(self, data):
         try:
             self.sender.send_data(data)
-            return self.receiever.capture_data()
+            response = self.receiever.capture_data()
+            if response.get("message") == "Success":
+                return response
+            else:
+                raise ValueError("GraspGen server failed.")
         except Exception as e:
             raise e
 
